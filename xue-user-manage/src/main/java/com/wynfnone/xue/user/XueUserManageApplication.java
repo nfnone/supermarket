80eccc012e4f1474a86c5172ec671c6b606203a1
wang <wynfnone@gmail.com>
2020-03-23 00:19:47 +0300

    加入用户管理模块

---
+++ xue-user-manage/src/main/java/com/wynfnone/xue/user/XueUserManageApplication.java
@@ -0,0 +1,13 @@
+package com.wynfnone.xue.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XueUserManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XueUserManageApplication.class, args);
+    }
+
+}
